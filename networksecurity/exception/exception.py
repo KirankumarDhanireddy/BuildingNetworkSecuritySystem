21865c4223d476f9b599222ddd3ac37225cd1989
kirankumardhanireddy <kirankumardhanireddy@gmail.com>
2025-11-03 12:01:37 +0530

    Add exception handling and logging modules for network security system

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,24 @@
+import sys
+from networksecurity.logging import logger
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        self.error_message = error_message
+        _,_, exc_tb = error_details.exc_info()
+
+        self.line_number = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return "Error occurred in script: {0} at line number: {1} with message: {2}".format(
+            self.file_name, 
+            self.line_number, 
+            str(self.error_message))
+    
+if __name__ == "__main__":
+    try:
+        logger.logging.info("Testing NetworkSecurityException")
+        a = 1 / 0
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+            
